02ce41f161ef38879aac3894ca6bb796c2e2ac8f
liujunyuan <ljyuan@bupt.edu.cn>
2020-12-02 21:14:43 +0800

    fix

--- gather_path.py
+++ gather_path.py
@@ -1,9 +1,9 @@
 import os
 # get .txt document
-rootdir=os.path.join('./Dataset/')
+rootdir = os.path.join('./Dataset/')
 # read
-write_path=open('./Dataset/ori_paths.txt', 'w')
-for (dirpath,dirnames,filenames) in os.walk(rootdir):
+write_path = open('./Dataset/ori_paths.txt', 'w')
+for (dirpath, dirnames, filenames) in os.walk(rootdir):
     for filename in filenames:
         if filename == 'IMU.txt':
             write_path.write(os.path.join(dirpath, filename) + '$')
